87a67067ba038b6457e89d9dbc500f3c53d610b2
Roman Goralchuk <gromka.vladyko@gmail.com>
2021-09-29 22:33:28 +0300

    The first servlet was started

--- src/main/java/by/it/academy/HelloServlet.java
+++ src/main/java/by/it/academy/HelloServlet.java
@@ -1,6 +1,5 @@
 package by.it.academy;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,12 +7,10 @@ import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
 
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException
+    public void doGet(HttpServletRequest request, HttpServletResponse resp)
+            throws IOException
     {
         resp.setContentType("text/html");
         resp.getWriter().write("Hello Servlet 1");
     }
-
 }
