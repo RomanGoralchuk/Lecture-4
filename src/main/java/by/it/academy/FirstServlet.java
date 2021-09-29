affc4bd4d31399a2e07defb4ccce1d344afcb28a
Roman Goralchuk <gromka.vladyko@gmail.com>
2021-09-30 01:05:17 +0300

    This task was completed thanks to Artyom Borisevich

--- src/main/java/by/it/academy/FirstServlet.java
+++ src/main/java/by/it/academy/FirstServlet.java
@@ -3,7 +3,9 @@ package by.it.academy;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class FirstServlet extends HttpServlet {
 
@@ -12,6 +14,19 @@ public class FirstServlet extends HttpServlet {
             throws IOException
     {
         resp.setContentType("text/html");
-        resp.getWriter().write("Hello First XML Servlet");
+        resp.getWriter().write("Hello First XML Servlet ");
+        HttpSession session = request.getSession();
+        Long visitCounter = (Long) session.getAttribute("visitCounter");
+        if (visitCounter == null) {
+            visitCounter = 1L;
+        } else {
+            visitCounter++;
+        }
+        session.setAttribute("visitCounter", visitCounter);
+
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.println("Page was visited " + visitCounter + " times."+"\n"+
+                    "Name of current Thread  "+Thread.currentThread().getName());
+        }
     }
 }
--- src/main/java/by/it/academy/SecondServlet.java
+++ src/main/java/by/it/academy/SecondServlet.java
@@ -1,19 +1,48 @@
 package by.it.academy;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet (name = "SecondServlet", urlPatterns = {"/second"})
+@WebServlet(name = "SecondServlet", urlPatterns = {"/second"})
 public class SecondServlet extends HttpServlet {
+    private Long accessServerCountGet;
+    private Long accessServerCountPost;
+
+    @Override
+    public void init() throws ServletException {
+        accessServerCountGet = 0L;
+        accessServerCountPost = 0L;
+    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse resp)
-            throws IOException
-    {
+            throws IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("Hello Second Annotations Servlet");
+        resp.getWriter().write("Hello Second Annotations Servlet ");
+        accessServerCountGet++;
+
+        try (PrintWriter printWriter = resp.getWriter()) {
+            printWriter.write("Hello I'm servlet  which uses annotation" + "\n" +
+                    "Access to server, count: " + accessServerCountGet + "\n" +
+                    "Name of current Thread: " + Thread.currentThread().getName());
+        }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        //someLogic
+        accessServerCountPost++;
+        try (PrintWriter printWriter = resp.getWriter()) {
+            printWriter.write("Access to server, count: " + accessServerCountPost + "\n" +
+                    "Name of current Thread: " + Thread.currentThread().getName());
+        }
+    }
+
+
 }
